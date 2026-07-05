32abc11b2f28d9817bdc39aed78ce1c403f9daba
nicholas Daniel <97566463+Jazzonies@users.noreply.github.com>
2026-07-05 06:06:07 +0100

    Simplify fetch call in script.js
    Removed redundant fetch options and corrected the form data variable.

--- script.js
+++ script.js
@@ -3,17 +3,11 @@ const form = new FormData();
 form.append("file", selectedImage);
 
 const response = await fetch(
-
-"https://danielnicholasdilli-weight-mdel.hf.space/predict",
-
-{
-
-method:"POST",
-
-body:form
-
-}
-
-);
+  "https://danielnicholasdilli-weight-mdel.hf.space/predict",
+  {
+      method: "POST",
+      body: formData
+  }
+)
 
 const data = await response.json();
